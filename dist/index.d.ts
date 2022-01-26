eaa723845d47cc328404aac6bfd05a339c46fa99
isaackwok <kch062522@gmail.com>
2022-01-26 23:54:11 +0800

    fix: ignore `dist` dir

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare type useRegexState = [
-    isMatch: boolean,
-    matches: RegExpMatchArray | null,
-    indexOfFirstMatch: number,
-    replace: (replaceValue: string) => () => string
-];
-export default function useRegex(regex: RegExp, targetString: string): useRegexState;
-export {};
